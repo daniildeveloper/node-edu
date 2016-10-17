a12498cad9513d9428efcc06b8904e8bade9b479
daniiltserin <daniilborowkow@yandex.ru>
2016-10-17 13:33:49 +0600

    Learned how to use handlebars-template engine on server-side

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,63 @@
 var express = require('express'),
-    handlebars = require('express-handlebars').create({ defaultLayout: 'main' });//use layout '/views/lauots/main.handlebars' as main(or root)
+    handlebars = require('express-handlebars').create({
+        defaultLayout: 'main',
+        helpers: {
+            //секции в отличие от макетов используются для внедрение в другие части макета
+            // идея почерпнута у Razor
+            section: function (name, options) {
+                if (!this._sections) {
+                    this._sections = {};
+                }
+                this._sections[name] = options.fn(this);
+                return null;
+            }
+        }
 
-var fortune = require('./lib/fortune');
+    });//use layout '/views/lauots/main.handlebars' as main(or root)
+
+var fortune = require('./lib/fortune'),
+    weather = require('./lib/weatherData');
 
 var app = express();
 app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.set('view engine', 'handlebars');   //used `handlebars` template engine
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
     next();
 });
-
+app.use(function (req, res, next) {
+    if (!res.locals.partials) {
+        res.locals.partials = {};
+    }
+    res.locals.partials.weatherContext = weather.getWeatherData();
+    next();
+});
 //marshrute
 app.get('/', function (req, res) {
     res.render('home');
 });
-app.get('/about', function (req, res) { 
+app.get('/about', function (req, res) {
     res.render('about', { fortune: fortune.getFortune, pageTestScript: '/qa/test-global.js' });
 });
 
 //tours
 app.get("tours/hood-river", function (req, res) {
-   res.render('tours/hood-river');
+    res.render('tours/hood-river');
 });
 app.get("tours/request-group-rate", function (req, res) {
-   res.render('tours/request-group-rate');
+    res.render('tours/request-group-rate');
+});
+
+//settings
+app.get('/headers', function (req, res) {
+    res.set('Content-type', 'text/plain');
+    var s = '';
+    for (var name in req.headers) {
+        s += name + ": " + req.headers[name] + "\n";
+    }
+    res.send(s);
 });
 //404
 app.use(function (req, res, next) {
